d6eed0d0a7d73de4149e39ff647133605d4802e8
vsk.mac-03 <asdsa@bk.ru>
2023-04-05 15:43:35 +1000

    123

--- app/src/main/java/com/example/myapplication/Registr_and_vhod.java
+++ app/src/main/java/com/example/myapplication/Registr_and_vhod.java
@@ -1,5 +1,14 @@
 package com.example.myapplication;
 
-public class Registr_and_vhod {
+import android.content.Intent;
+import android.widget.Button;
 
+public class Registr_and_vhod {
+    Button btN1;
+
+    public void onClick(){
+        Intent intent = new Intent(this, Cod.class);
+
+        startActivity(intent);
+    }
 }
