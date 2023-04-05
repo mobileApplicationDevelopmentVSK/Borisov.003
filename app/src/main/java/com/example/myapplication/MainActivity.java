473ec48486111e6507b03485bab01a666097a2c4
vsk.mac-03 <asdsa@bk.ru>
2023-04-05 14:44:35 +1000

    123

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        @Override
+                public void run(){
+            Intent i = new Intent( this, MainActivity2.class);
+            startActivity(i);
+            finish();
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity2{
+    TextView some_id;
+
+//    @Override
+//    public void onClick(View v) {
+//        some_id = (TextView) findViewById(R.id.some_id);
+//        some_id.OnClick();
+//    }
+
+}
